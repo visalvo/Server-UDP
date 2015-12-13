3899121b84601443039f316ae48135448c8e75ff
lateg <lategano.antonio@gmail.com>
2015-12-13 10:26:52 +0100

    First release.

--- server.c
+++ server.c
@@ -13,8 +13,9 @@
 
 #include <stdio.h>
 #include <string.h> //for memset()
+#include <ctype.h>
 
-#define ECHOMAX 255
+#define DIM_MSG 255
 #define PORT 18000
 
 void errorHandler(char *msg) {
@@ -25,6 +26,11 @@ void clearWinSock() {
 #if defined WIN32
     WSACleanup();
 #endif
+}
+
+void closeConnection(int socket) {
+    closesocket(socket);
+    clearWinSock();
 }
 
 int main() {
@@ -41,12 +47,15 @@ int main() {
     struct sockaddr_in serverAddr;
     struct sockaddr_in clientAddr;
     unsigned int clientAddrLen;  //lunghezza dell'indirizzo
-    char buffer[ECHOMAX];
-    int recvMsgSize;  //dimensione del messaggio
+    int msg_len;  //dimensione del messaggio
+    char buffer[DIM_MSG];  //buffer per la ricezione
+    int recvMsgSize;  //dimensione del messaggio ricevuto
 
     //CREAZIONE DELLA SOCKET
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         errorHandler("socket() failed!");
+        clearWinSock();
+        return 0;
     }
 
     // COSTRUZIONE DELL'INDIRIZZO DEL SERVER
@@ -58,29 +67,64 @@ int main() {
     // BIND DELLA SOCKET
     if ((bind(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr))) < 0) {
         errorHandler("bind() failed!");
+        closeConnection(sock);
+        return 0;
     }
 
-    // RICEZIONE DELLA STRINGA ECHO DAL CLIENT
+
     while (1) {
+        // RICEZIONE DEL MESSAGGIO INIZIALE DEL CLIENT CON IL NOME DELL'HOST
         clientAddrLen = sizeof(clientAddr);
-        recvMsgSize = recvfrom(sock, buffer, ECHOMAX, 0, (struct sockaddr *) &clientAddr, &clientAddrLen);
-        printf("Handling client %s\n", inet_ntoa(clientAddr.sin_addr));
-        printf("Received: %s \n", buffer);
-
-        // RINVIA LA STRINGA ECHO AL CLIENT
-        if (sendto(sock, buffer, recvMsgSize, 0, (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != recvMsgSize) {
-            errorHandler("sendto() sent different number of bytes than expected!");
+        if ( (recvMsgSize = recvfrom(sock, buffer, DIM_MSG, 0, (struct sockaddr *) &clientAddr, &clientAddrLen)) <= 0) {
+            errorHandler("recvfrom() receive different number of bytes than expected!");
+            closeConnection(sock);
+            return 0;
         }
+        buffer[recvMsgSize] = '\0';
+        printf("Message: '%s'\n", buffer);
+
+        struct in_addr addr;
+        char *s = inet_ntoa(clientAddr.sin_addr);
+        addr.s_addr = inet_addr(s);
+        struct hostent *host = gethostbyaddr((char*) &addr, 4, AF_INET);
+        printf("Received from client with host name: '%s'\n", host->h_name);
+
+        //INVIO DEL MESSAGGIO OK AL CLIENT
+        char *msg = "OK";
+        msg_len = strlen(msg);
+        if (sendto(sock, msg, msg_len, 0, (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != msg_len) {
+            errorHandler("sendto() sent different number of bytes than expected!");
+            closeConnection(sock);
+            return 0;
+        }
+
+        //RICEZIONE VOCALI DAL CLIENT
+        printf("Vocali ricevute: ");
+        //char str_vow[DIM_MSG];
+        char c;
+        //int i = 0;
+        int ans = 1;
+        while(ans) {
+            if ( (recvMsgSize = recvfrom(sock, buffer, 1, 0, (struct sockaddr *) &clientAddr, &clientAddrLen)) <= 0) {
+                errorHandler("recvfrom() receive different number of bytes than expected!");
+                closeConnection(sock);
+                return 0;
+            }
+
+            if(buffer[0] == '\n') {
+                ans = 0;
+            }
+            else {
+                c = toupper(buffer[0]);
+                if (sendto(sock, &c, 1, 0, (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != 1) {
+                    errorHandler("sendto() sent different number of bytes than expected!");
+                    closeConnection(sock);
+                    return 0;
+                }
+            }
+        }
+
+        system("pause");
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
